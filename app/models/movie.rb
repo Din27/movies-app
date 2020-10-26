33dfa99f9d582431c0841845dc137d7218851e14
Dmytro Chelyadin <dmytro_chelyadin@epam.com>
2020-10-26 19:48:50 +0200

    Added validation for Movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :year, presence: true,
+            inclusion: { in: 1900..Date.today.year+5 },
+            format: {
+                with: /(19|20)\d{2}/i,
+                message: "Should be a correct year"
+            }
 end
