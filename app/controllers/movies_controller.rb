f8ac9f66d97856930d8f080bcd199329ecde56cd
Dmytro Chelyadin <dmytro_chelyadin@epam.com>
2020-10-26 19:54:40 +0200

    Added Movie validation in Controller + messages on UI

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,15 +9,18 @@ class MoviesController < ApplicationController
   end
 
   def new
+    @movie = Movie.new
   end
 
   def create
     @movie = Movie.new(movie_params)
 
-    @movie.save
-    redirect_to @movie
+    if @movie.save
+      redirect_to @movie
+    else
+      render 'new'
+    end
   end
-
 
   private
 
