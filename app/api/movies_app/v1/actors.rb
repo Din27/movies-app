74f7a5ec19039b859c04c2dcdfd6909b0fe7889f
Dmytro Chelyadin <dmytro_chelyadin@epam.com>
2020-10-28 15:46:49 +0200

    Extended API with updating/deleting/creating movies and additional param for reading associated entities for movies and actors

--- app/api/movies_app/v1/actors.rb
+++ app/api/movies_app/v1/actors.rb
@@ -6,29 +6,37 @@ module MoviesApp
       prefix :api
 
       resource :actors do
+
+        desc 'Return list of actors.'
+        get do
+          Actor.all
+        end
+
         desc 'Return an actor.'
         params do
           requires :id, type: Integer, desc: 'Actor ID.'
+          optional :include_movies, type: Boolean, default: false, desc: 'Includes list of movies if set to true'
         end
         route_param :id do
           get do
             actor = Actor.find_by(id: params[:id])
             if actor.present?
-              actor
+              if params[:include_movies]
+                actorWithMovies = actor.attributes
+                actorWithMovies[:movies] = actor.movies
+                actorWithMovies
+              else
+                actor
+              end
             else
               error! :not_found, 404
             end
           end
         end
-      end
 
-      resource :actors do
-        desc 'Return list of actors.'
-        get do
-          Actor.all
-        end
-      end
+        # TODO add methods
 
+      end
     end
   end
 end
--- app/api/movies_app/v1/movies.rb
+++ app/api/movies_app/v1/movies.rb
@@ -6,29 +6,92 @@ module MoviesApp
       prefix :api
 
       resource :movies do
+        desc 'Return list of movies.'
+        get do
+          Movie.all
+        end
+
         desc 'Return a movie.'
         params do
           requires :id, type: Integer, desc: 'Movie ID.'
+          optional :include_actors, type: Boolean, default: false, desc: 'Includes list of actors if set to true.'
         end
         route_param :id do
           get do
+            # using find_by over find to be able to test if movie is found without causing an exception
+            # or should I instead rescue the exception and then map it to the status?
             movie = Movie.find_by(id: params[:id])
             if movie.present?
-              movie
+              if params[:include_actors]
+                # if include_actors param was passed as true, get a hash of movie attrs and extend it with actors data
+                # TODO was not sure what the default behavior should be - return actors or not, or make it two separate calls, so went with query param
+                movieWithActors = movie.attributes
+                movieWithActors[:actors] = movie.actors
+                movieWithActors
+              else
+                # otherwise just send movie attributes as is without actors data
+                movie
+              end
             else
               error! :not_found, 404
             end
           end
         end
-      end
 
-      resource :movies do
-        desc 'Return list of movies.'
-        get do
-          Movie.all
+        desc 'Create a movie.'
+        params do
+          requires :title, type: String, desc: 'Movie title.'
+          requires :year, type: Integer, desc: 'Movie year of creation.'
         end
-      end
+        post do
+          @movie = Movie.new(title: params[:title], year: params[:year])
 
+          if @movie.save
+            status 201
+            # TODO Location header
+          else
+            # TODO it can probably be 500. how to distinguish?
+            error! :failed_to_create, 400
+          end
+        end
+
+        desc 'Update a movie. Do NOT create a movie if id is not found.'
+        params do
+          requires :id, type: Integer, desc: 'Movie ID.'
+          requires :title, type: String, desc: 'Movie title.'
+          requires :year, type: Integer, desc: 'Movie year of creation.'
+        end
+        put ':id' do
+          movie = Movie.find_by(id: params[:id])
+          if movie.present?
+            if movie.update({ title: params[:title], year: params[:year] })
+              status 204
+            else
+              # TODO it can probably be 500. how to distinguish?
+              error! :failed_to_update, 400
+            end
+          else
+            error! :not_found, 404
+          end
+        end
+
+        desc 'Delete a movie.'
+        params do
+          requires :id, type: Integer, desc: 'Movie ID.'
+        end
+        delete ':id' do
+          movie = Movie.find_by(id: params[:id])
+          if movie.present?
+            movie.destroy
+            status 204
+          else
+            error! :not_found, 404
+          end
+        end
+
+        # TODO add methods to add/remove actors to/from the movie
+
+      end
     end
   end
 end
